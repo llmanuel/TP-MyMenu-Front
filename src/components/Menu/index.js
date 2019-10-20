1b3c96f8256a0513f46d4f025cba18b314ef0d0a
llmanuel <llauromanuel@gmail.com>
2019-10-20 14:32:18 -0300

    Feat: Creation of left menu component to navigate through the app

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,33 +1,41 @@
 import React from 'react';
+import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Dish from '../Dish';
+import Meals from '../Meals';
+import DailyMenu from '../DailyMenu';
+import Beverages from '../Beverages';
 
 import styles from './styles.module.scss';
 
-const defaultProps = {
-    title: "Hamburguesa",
-    image: "images/burger.png",
-    description: "Deliciosa hamburguesa de entraña",
-};
-
 const Menu = () => (
-  <div className={styles.display}>
-    <Dish 
-        title={defaultProps.title}
-        image={defaultProps.image}
-        description={defaultProps.description}
-    />
-    <Dish 
-        title={defaultProps.title}
-        image={defaultProps.image}
-        description={defaultProps.description}
-    />
-    <Dish 
-        title={defaultProps.title}
-        image={defaultProps.image}
-        description={defaultProps.description}
-    />
-  </div>
+  <Router>
+    <div className={styles.menuBackground}>
+        <div className={styles.box}>
+          <ul className={styles.menu}>
+            <li className={styles.menuOption}>
+              <Link to="/meals" className={styles.link}>
+                <h4>Menu</h4>
+              </Link>
+            </li>
+            <li className={styles.menuOption}>
+              <Link to="/daily-menu" className={styles.link}>
+                <h4>Menu del dia</h4>
+              </Link>
+            </li>
+            <li className={styles.menuOption}>
+              <Link to="/beverages" className={styles.link}>
+                <h4>Bebidas</h4>
+              </Link>
+            </li>
+          </ul>
+        </div>
+    </div>
+    <Switch>
+      <Route exact path="/meals" component={Meals} />
+      <Route exact path="/daily-menu" component={DailyMenu} />
+      <Route exact path="/beverages" component={Beverages} />
+    </Switch>
+  </Router>
 );
 
 export default Menu;
